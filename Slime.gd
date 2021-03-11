884a2110f576ca456ea8fe130b252739751b8ae1
OliJeffery <o.jeffery@gmail.com>
2021-03-11 22:50:29 +0000

    Monsters find position relative to player on grid

--- Slime.gd
+++ Slime.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var moving = false
 var player_position
+var monster_position
 var grid_size
 var direction
 
@@ -11,8 +12,24 @@ func _process(delta):
 		moving = false
 
 func move_monster():
-	var monster_position = global_position/16
+	gridify()
+	if monster_position.y > player_position.y:
+		print('MOVING UP')
+	elif monster_position.y < player_position.y:
+		print('MOVING DOWN')
+	elif monster_position.x > player_position.x:
+		print('MOVING LEFT')
+	elif monster_position.x > player_position.x:
+		print('MOVING RIGHT')
+	else:
+		print('NOM')
+	
+func gridify():
+	monster_position = global_position/grid_size
 	monster_position.x = floor(monster_position.x) + 1
 	monster_position.y = floor(monster_position.y) + 1
-	print(monster_position)
+	player_position = player_position/grid_size
+	player_position.x = floor(player_position.x) + 1
+	player_position.y = floor(player_position.y) + 2
+
 	 
